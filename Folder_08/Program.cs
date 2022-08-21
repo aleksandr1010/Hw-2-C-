af6c0d0b1fb5b5534abb81c1660311838a1af0ac
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 18:47:17 +0300

    Task_08

---
+++ Folder_08/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число:   ");
+int n = int.Parse(Console.ReadLine());
+
+
+int result (int x)
+{
+    int a = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        a = a * i;
+    }
+    return a;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {n} равно {result(n)}");
+Console.WriteLine();
