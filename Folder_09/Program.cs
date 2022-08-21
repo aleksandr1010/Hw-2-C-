9e731fa1b4737647b3fae31e659f0a0d9982e6d9
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 18:49:41 +0300

    Task_09

---
+++ Folder_09/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число :");
+int N = int.Parse(Console.ReadLine());
+int number = 1;
+Console.WriteLine("Ответ :");
+while (number <= N)
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0) Console.WriteLine($"{number} ^ 3 = {cube}" );
+    number++;
+   
+}
+Console.WriteLine();
