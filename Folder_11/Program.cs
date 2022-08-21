e2d9fed2cc9df0499079c0d0c5241c9d25b9c5f7
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 19:11:12 +0300

    Task_11

---
+++ Folder_11/Program.cs
@@ -0,0 +1,11 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число : ");
+int Number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Дано число: {Number}");
+
+if (Number / 100 > 0)Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+else Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+Console.WriteLine();
