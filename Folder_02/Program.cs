d625ca85f506eacfabbd4c97e7ca5769e94541c6
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 18:27:22 +0300

    Task_02

---
+++ Folder_02/Program.cs
@@ -0,0 +1,22 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("введите число 1");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 = int.Parse(Console.ReadLine());
+int quadro1 = Quadro(number1);
+int quadro2 = Quadro(number2);
+
+if (quadro1 == number2 || quadro2 == number1)
+{
+Console.Write("Да");
+}
+else 
+{
+  Console.Write("Нет");  
+}
+
+int Quadro (int a)
+{
+    return a * a;
+} 
