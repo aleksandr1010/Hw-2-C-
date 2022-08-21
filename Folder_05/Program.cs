b90cc47a455c7c4e0ab3b3fae37a6390080190ce
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 18:38:16 +0300

    Task_05

---
+++ Folder_05/Program.cs
@@ -0,0 +1,11 @@
+﻿//Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+int result = 0;
+for (int i = 1; i <= A; i++)
+{
+	result += i;
+}
+Console.WriteLine(result);
+
