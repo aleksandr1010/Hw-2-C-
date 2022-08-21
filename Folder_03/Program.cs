409a2c84a9704eeb3e8a8ecf09d5a0916df2c266
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 18:30:38 +0300

    Task_03

---
+++ Folder_03/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: ");
+int quarterNum = int.Parse(Console.ReadLine());
+
+string range = "";
+string num = "";
+
+if      (quarterNum == 1) range = "x > 0, y > 0";
+else if (quarterNum == 2) range = "x < 0, y > 0";
+else if (quarterNum == 3) range = "x < 0, y < 0";
+else  range = "x > 0, y < 0";
+
+Console.WriteLine($"Для {quarterNum}-й четверти координаты точки должны быть заданы в диапазоне ( {range} )");
+Console.WriteLine();
