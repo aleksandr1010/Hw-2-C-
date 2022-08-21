4cf4d1db9d4a32165e245bff85dc36ac070cd376
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 19:08:30 +0300

    Task_10

---
+++ Folder_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int Number = new Random().Next(10, 100);
+int Max = 0;
+
+int MaxDigit()
+{
+    int DigitOne = Number / 10;
+    int DigitTwo = Number % 10;
+    if (DigitOne > DigitTwo) Max = DigitOne;
+    else Max = DigitTwo;
+
+    return Max;
+}
+
+MaxDigit();
+Console.WriteLine();
+Console.WriteLine($"В числе {Number} наибольшая цифра {Max}");
+Console.WriteLine();
