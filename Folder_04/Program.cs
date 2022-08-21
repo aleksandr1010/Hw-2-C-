4e9b7c95f88323cf73b0fe85f130d18f9be61eb9
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-21 18:35:41 +0300

    Task_04

---
+++ Folder_04/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число :");
+int N = int.Parse(Console.ReadLine());
+int number = 1;
+Console.WriteLine("Ответ :");
+while (number <= N)
+{
+    int cube = number * number * number;
+    Console.WriteLine($"{number} ^ 3 = {cube}" );
+    number++;
+   
+}
+Console.WriteLine();
